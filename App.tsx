9879b8abb65dc3dff6142c1dadce12195ccebc31
Leonardo Ribeiro <leonardo.rib@hotmail.com>
2022-10-16 16:32:23 -0300

    Added github example

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import CollapsibleNavBar from './CollapsibleNavBar';
+import AirbnbHeaderExample from './AirbnbHeaderExample';
 
 const App = () => {
   return (
